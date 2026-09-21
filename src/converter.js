dc25815e93228656ef2b92a26e9fccd867cdac94
UrsidaeDevise <magnusson.bjorn@gmail.com>
2026-09-21 12:14:11 +0200

    Added correct bit shift, java script does not to the AND (&) operator correct for 32 bit unsigned number, resulting it becoming negative in some cases.

--- src/converter.js
+++ src/converter.js
@@ -8,7 +8,7 @@ let parseFrame = function (parserLookup, data) {
     data = Number(data >> BigInt(bitShift));
     data = data & (2 ** parserLookup.bitLength - 1);
 
-    data = (data * parserLookup.factor + parserLookup.offset);
+    data = (data & ((2 ** parserLookup.bitLength) - 1))  >>> 0;
     return {
         path: parserLookup.name,
         value: data,
